7cfb59649e4bc5ad52be6fa993841da0f4d1b2f3
lmorand <lmorand@users.noreply.github.com>
2022-08-15 00:54:57 -0300

    8. Diffuse Materials

--- raytracer.py
+++ raytracer.py
@@ -6,20 +6,26 @@ from sphere import Sphere
 from hittable_list import HittableList
 from camera import Camera
 
-def ray_color(ray, world):
-	hit_rec = world.hit(ray, 0, np.inf)
+def ray_color(ray, world, depth):
+	# If we've exceeded the ray bounce limit, no more light is gathered.
+	if depth <= 0:
+		return Color(0,0,0)
+
+	hit_rec = world.hit(ray, 0.001, np.inf)
 	if hit_rec is not None:
-		n = hit_rec[2]
-		return 0.5*Color(*(n+1))
+		p, normal = hit_rec[1:3]
+		target = p + normal + random_unit_vector()
+		return 0.5*ray_color(Ray(p, target - p), world, depth-1)
 	
 	unit_direction = unit_vector(ray.direction())
 	t = 0.5 * (unit_direction[Y] + 1.0)
 	return (1.0-t)*Color(1.0, 1.0, 1.0) + t*Color(0.5, 0.7, 1.0)
 
 aspect_ratio = 16.0 / 9.0
-image_width = 400
+image_width = 200
 image_height = round(image_width / aspect_ratio)
 samples_per_pixel = 100
+max_depth = 5
 
 world = HittableList()
 world.add(Sphere(Point3(0,0,-1), 0.5))
@@ -28,17 +34,22 @@ world.add(Sphere(Point3(0,-100.5,-1), 100))
 cam = Camera()
 image = []
 
-for j in range(image_height-1, -1, -1):
-	print(f'Scanlines remaining: {j:4}', end='\r')
-	for i in range(image_width):
-		pixel_color = Color(0, 0, 0)
-		for s in range(samples_per_pixel):
-			u = (i + rng.random()) / (image_width-1)
-			v = (j + rng.random()) / (image_height-1)
-			r = cam.get_ray(u, v)
-			pixel_color += ray_color(r, world)
-		image.append(format_color(pixel_color, samples_per_pixel))
-print('\nDone.')
+try:
+	for j in range(image_height-1, -1, -1):
+		print(f'Scanlines remaining: {j:4}', end='\r')
+		for i in range(image_width):
+			pixel_color = Color(0, 0, 0)
+			for s in range(samples_per_pixel):
+				u = (i + rng.random()) / (image_width-1)
+				v = (j + rng.random()) / (image_height-1)
+				r = cam.get_ray(u, v)
+				pixel_color += ray_color(r, world, max_depth)
+			image.append(format_color(pixel_color, samples_per_pixel))
+	print('\nDone.')
 
-image = np.array(image, dtype=np.uint8).reshape((image_height, image_width, 3))
-Image.fromarray(image, 'RGB').save('out.png')
+	image = np.array(image, dtype=np.uint8).reshape((image_height, image_width, 3))
+	Image.fromarray(image, 'RGB').save('out.png')
+
+except KeyboardInterrupt:
+	image = np.array(image[:(image_height-j-1)*image_width], dtype=np.uint8).reshape((image_height-j-1, image_width, 3))
+	Image.fromarray(image, 'RGB').save('partial_out.png')
--- utility.py
+++ utility.py
@@ -2,23 +2,40 @@ import numpy as np
 from numpy.random import default_rng
 
 def Vec3(x, y, z):
-    return np.array([x, y, z], dtype=np.float64)
+	return np.array([x, y, z], dtype=np.float64)
 
 def Color(r, g, b):
-    return Vec3(r, g, b)
+	return Vec3(r, g, b)
 
 def Point3(u, v, w):
-    return Vec3(u, v, w)
+	return Vec3(u, v, w)
 
 def unit_vector(vec):
 	return vec / np.linalg.norm(vec)
 
-def format_color(pixel_color, samples_per_pixel):
-    # Divide the color by the number of samples.
-    scaled_pixel_color = pixel_color / samples_per_pixel
+def random_in_unit_sphere():
+	while True:
+		p = rng.uniform(-1.0, 1.0, 3)
+		if np.dot(p, p) >= 1:
+			continue
+		return p
 
-    # Write the translated [0,255] value of each color component.
-    return 256 * np.clip(scaled_pixel_color, 0.0, 0.999)
+def random_unit_vector():
+	return unit_vector(random_in_unit_sphere())
+
+def random_in_hemisphere(normal):
+    in_unit_sphere = random_in_unit_sphere()
+    if np.dot(in_unit_sphere, normal) > 0.0: # In the same hemisphere as the normal
+        return in_unit_sphere
+    else:
+        return -in_unit_sphere
+
+def format_color(pixel_color, samples_per_pixel):
+	# Divide the color by the number of samples and gamma-correct for gamma=2.0.
+	scaled_pixel_color = np.sqrt(pixel_color / samples_per_pixel)
+
+	# Write the translated [0,255] value of each color component.
+	return 256 * np.clip(scaled_pixel_color, 0.0, 0.999)
 
 X = R = 0
 Y = G = 1
